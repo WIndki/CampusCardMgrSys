97f83c383be6cff0d32f448182bfa07f5493b185
Windki <i@windki.com>
2024-11-20 21:29:18 +0800

    statics静态文件

--- main/mainsys/urls.py
+++ main/mainsys/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('login/', views.userLogin, name='login'),
     path('logout/',views.userLogout, name='logout'),
     path('register/', views.userRegister, name='register'),
